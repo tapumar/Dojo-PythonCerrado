8c380e35b7f163200462f61d1bae7a27d85311b2
Talitha Pumar <tapumar@gmail.com>
2018-09-30 02:53:37 -0100

    Update main.py

--- Dojo-PythonCerrado/main.py
+++ Dojo-PythonCerrado/main.py
@@ -1,7 +1,11 @@
 class Brainfuck:
     def brainfuck_program(self,program):
         idx = 0
+        cells = [0] * 30000
+        pointer = 0
         while idx < len(program):
-    			op = program[idx]
-    			if op == '+':
-    				cells[pointer] += 1
+            op = program[idx]
+            if op == '+':
+                cells[pointer] += 1
+            idx += 1
+        return cells[pointer]
